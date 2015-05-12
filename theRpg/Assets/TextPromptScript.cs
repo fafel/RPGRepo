53da7a2576a4d2259f51342ab4c089c4e9647e13
ghit <tmaxkern@gmail.com>
2015-05-12 15:02:02 -0700

    TUESDAY
    GOING UP ON A TUESDAY

---
+++ theRpg/Assets/TextPromptScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TextPromptScript : MonoBehaviour {
+
+	public float radius;
+	public string text;
+	public LayerMask player;
+	public Text textT;
+
+	bool stoped = false;
+
+	void Start(){
+		textT.text = "";
+	}
+
+
+	void FixedUpdate(){
+		Collider2D col = Physics2D.OverlapCircle (transform.position, radius, player);
+		if (col != null) {
+			stoped = true;
+			Time.timeScale = 0.0f;
+			textT.text = text;
+		}
+	}
+
+	void Update(){
+		if (stoped) {
+			if (Input.GetMouseButtonDown(0)){
+				if (Input.mousePosition.y < 70 ){
+					textT.text = "";
+					Time.timeScale = 1.0f;
+					stoped = false;
+					Destroy (transform.gameObject);
+				}
+			}
+
+		}
+	}
+
+
+
+
+
+
+
+
+}
