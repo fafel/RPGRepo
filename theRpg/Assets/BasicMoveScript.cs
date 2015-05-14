7e02fcc4e8698ecc86c45e9e07e6878af111cfc2
ghit <tmaxkern@gmail.com>
2015-05-14 12:12:18 -0700

    Final pre trip comit
    asdfasdfads

--- theRpg/Assets/BasicMoveScript.cs
+++ theRpg/Assets/BasicMoveScript.cs
@@ -6,20 +6,24 @@ public class BasicMoveScript : MonoBehaviour {
 
 	private Animator anim;
 
-	public enum Type {UpDown, LefRight, Circle} 
+	public enum Type {UpDown, LefRight, Circle, TargetMode} 
 	public Type type;
 	public int length;
 	public float speed;
+	public LayerMask player;
 
 	Vector3 p1;
 	Vector3 p2;
 	private bool there;
+
+	Vector3 target;
 
 	float x = 0;
 
 
 	void Start(){
 		anim = transform.gameObject.GetComponent<Animator> ();
+		target = transform.position;
 		if (type.Equals (Type.UpDown)) {
 			p1 = transform.position;
 			p2 = transform.position + new Vector3 (0, length, 0);
@@ -68,6 +72,16 @@ public class BasicMoveScript : MonoBehaviour {
 			//if (
 
 			break;
+		case Type.TargetMode:
+			if (Mathf.Abs (target.x - transform.position.x) < speed * 5 && 
+			    Mathf.Abs (target.y - transform.position.y) < speed * 5)
+				transform.position = Vector3.MoveTowards (transform.position, target, 5 * speed * Time.deltaTime);
+			break;
+		}
+		Collider2D c = Physics2D.OverlapCircle (transform.position, 1.5f, player);
+		if (c != null) {
+			target = c.gameObject.transform.position;
+			type = Type.TargetMode;
 		}
 	}
 
--- theRpg/Assets/Scripts/ProjectileScript.cs
+++ theRpg/Assets/Scripts/ProjectileScript.cs
@@ -8,23 +8,31 @@ public class ProjectileScript : MonoBehaviour {
 	public int speed;
 	public Transform explosion;
 	public LayerMask enemy;
+	public float fuse;
 
 	void Start(){
 		//direction.Normalize ();
+		fuse = 0.03f;
 	}
 
 	void Update(){
+		fuse -= Time.deltaTime;
 		transform.position = transform.position + (direction * Time.deltaTime) * speed;
 		Collider2D col = Physics2D.OverlapCircle (transform.position, .05f, enemy);
-		if (col != null) {
+		if (col != null && fuse < 0) {
 			HealthScript hs = col.gameObject.GetComponent<HealthScript>();
 			if (hs != null){
 				hs.Damage(10);
 			}
 			Transform t = Instantiate(explosion) as Transform;
 			t.position = transform.position;
+			Explode();
 			Destroy(transform.gameObject);
 		}		
+
+	}
+
+	public virtual void Explode() {
 
 	}
 
