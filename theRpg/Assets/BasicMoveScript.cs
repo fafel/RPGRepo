641653bd8105beb248589c7eb27714caa91cc520
ghit <tmaxkern@gmail.com>
2015-05-14 10:39:16 -0700

    Play Fix
    Fixed stuff

--- theRpg/Assets/BasicMoveScript.cs
+++ theRpg/Assets/BasicMoveScript.cs
@@ -6,7 +6,7 @@ public class BasicMoveScript : MonoBehaviour {
 
 	private Animator anim;
 
-	public enum Type {UpDown, LefRight} 
+	public enum Type {UpDown, LefRight, Circle} 
 	public Type type;
 	public int length;
 	public float speed;
@@ -14,6 +14,8 @@ public class BasicMoveScript : MonoBehaviour {
 	Vector3 p1;
 	Vector3 p2;
 	private bool there;
+
+	float x = 0;
 
 
 	void Start(){
@@ -28,7 +30,6 @@ public class BasicMoveScript : MonoBehaviour {
 	}
 
 	void Update(){
-
 		switch (type) {
 		case Type.LefRight:
 			if (there){
@@ -46,9 +47,28 @@ public class BasicMoveScript : MonoBehaviour {
 			}
 			break;
 		case Type.UpDown:
+			if (there){
+				transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+				if (transform.position.y > p2.y){
+					there = false;
+					anim.SetTrigger("Run");
+				}
+			}else {
+				transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
+				if (transform.position.y < p1.y){
+					there = true;
+					anim.SetTrigger("RunUp");
+				}
+			}
+			break;
+		case Type.Circle:
+			x += Time.deltaTime;
+
+			transform.position += new Vector3(Mathf.Sin(5*x), Mathf.Cos(5*x), 0) / 100;
+			//if (
+
 			break;
 		}
-
 	}
 
 
